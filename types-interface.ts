8d2977f66dfc12bee31d625be0c7d5a672a6081c
Saurav Kumar <thekumar101@gmail.com>
2020-06-27 16:58:43 +0530

    Day0 | Exercise/Demo Code (TypeScript - intro, types, modules)

---
+++ types-interface.ts
@@ -0,0 +1,43 @@
+export interface Address {
+    apartment: number;
+    street: string;
+    city: string;
+    state: string;
+    pin: number;
+}
+
+export interface Customer {
+    customerId: string;
+    firstName: string;
+    lastName: string;
+    username: string;
+    password: string;
+    dateOfBirth: Date;
+    userTypeId: string;
+    totalBookings: number;
+    address: Address
+}
+
+let firstName: string = "John";
+let lastName: string = "Doe";
+let age: number = 22;
+let address: Address = {
+    apartment: 101,
+    street: "Fort Street",
+    city: "Mumbai",
+    pin: 400001,
+    state: "Maharashtra",
+};
+
+const getAddress = (address:  Address): Address => {
+    const addressString = `Apartment: ${address.apartment}, Street:  ${address.street}, City: ${address.city}, PIN: ${address.pin}, State: ${address.state}`;
+    return address;
+}
+
+const updateAddress = (address:  Address): Address => {
+    address.street = address.street.toUpperCase() === 'FORT STREET' ? 'Kings Street' : address.street;
+    return address;
+}
+
+getAddress(address);
+updateAddress(address);
